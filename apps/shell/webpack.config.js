37f879446ab3366df4091d627c1412dbf17a3f0b
Rares Matei <matei.rar@gmail.com>
2021-04-25 20:47:33 +0100

    fix plugin ts

--- apps/shell/webpack.config.js
+++ apps/shell/webpack.config.js
@@ -1,5 +1,5 @@
 const nrwlConfig = require('@nrwl/react/plugins/webpack.js'); // require the main @nrwl/react/plugins/webpack configuration function.
-const BuildableLibsPlugin = require('./buildable-libs-plugin');
+const BuildableLibsPlugin = require('./plugin/buildable-libs-plugin');
 
 module.exports = (config) => {
   nrwlConfig(config);
